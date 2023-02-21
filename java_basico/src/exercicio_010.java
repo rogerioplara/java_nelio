5d9ead057c5d43caebe285278ddac308aa308af8
Rogerio Lara <rogerioo.lara@hotmail.com>
2023-02-21 17:58:26 -0300

    Utilizando intellij

---
+++ java_basico/src/exercicio_010.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio_010 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] vet = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			vet[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < n; i++) {
+			if (vet[i] < 0) {
+				System.out.println(vet[i]);
+			}
+		}
+		
+		sc.close();
+	}
+
+}

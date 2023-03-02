536278da027cc0325cfc5b5f67b18d5b8ac95cd4
Usuário <Usurio@DESKTOP-OBUVFD8>
2023-03-01 22:46:40 -0300

    inicio exercicio listas

---
+++ exeercicio_list/application/Program_014.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Funcionarios;
+
+public class Program_014 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("How many empoyees will be registered: ");
+		int n = sc.nextInt();	
+		
+		//instancia da lista;
+		List<Funcionarios> func = new ArrayList<>();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.printf("Employee #%d%n", i + 1);
+			
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			
+			//iniiando o objeto;
+			Funcionarios funcionario = new Funcionarios(id, name, salary);
+			
+			//adicionando a lista;
+			func.add(funcionario);
+		}
+		
+		System.out.println();
+		System.out.println("Enter the employee id that will have salary increase: ");		
+		Integer idSearch = sc.nextInt();
+		
+		System.out.println("Enter increase percentage: ");
+		Double percentage = sc.nextDouble();
+		
+		List<Funcionarios> result = func.stream().filter(x -> )
+		
+		sc.close();
+	}
+
+}
---
+++ exeercicio_list/entities/Funcionarios.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Funcionarios {
+
+	private Integer id;
+	private String name;
+	private Double salary;
+	
+	public Funcionarios() {
+		
+	}
+	
+	public Funcionarios(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+	
+	public void increaseSalary(double percentage) {
+		this.salary += getSalary() * (percentage / 100);
+	}
+
+	public String toString() {
+		return String.format("%d, %s, %.2f%n", getId(), getName(), getSalary());
+	}
+	
+	
+
+}
